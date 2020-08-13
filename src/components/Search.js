1b6485c5f9105888066052291f4a2543a79fd911
szabinakov <szabinkov@gmail.com>
2020-08-13 13:55:26 +0100

    Added Search component

---
+++ src/components/Search.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import '../styles/Search.css'
+
+
+const Search = () => {
+    return(
+    <>
+        <input
+            className='searchInput'
+            type='text'>
+        </input>
+    </>
+    )
+}
+
+export default Search
