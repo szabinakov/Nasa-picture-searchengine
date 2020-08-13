6f72b2ff5668a20bb439bf37f975cba03673d0db
szabinakov <szabinkov@gmail.com>
2020-08-13 13:56:12 +0100

    rendering Search component in App

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import '../styles/App.css';
 
+import Search from './Search'
+
 function App() {
   return (
     <div className="App">
-      <p>Hello World!</p>
+      <img 
+      className='nasaLogo'
+      src=' https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg' 
+      alt='nasalogo'/>
+      <Search/>
     </div>
   );
 }
