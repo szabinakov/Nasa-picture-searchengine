707f45ce829b1f07d684b0921c0882e2693a58f2
szabinakov <szabinkov@gmail.com>
2020-08-13 14:28:26 +0100

    getting array of images with getimages function

--- src/requests/getImages.js
+++ src/requests/getImages.js
@@ -7,7 +7,10 @@ const getImages = (query) => {
         return axios
         .get(`https://images-api.nasa.gov/search?q=${query}`)
         .then((response) => {
-            console.log(response)
+            const imageResults = response.data.collection.items
+            const parsedImages = imageResults.filter((image) => image.data[0].media_type === 'image')
+            const images = parsedImages.map((image) => image.links[0].href)
+            return images
         })
         .catch((err) => {
             console.log(err)
