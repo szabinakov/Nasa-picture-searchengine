9dfe3f0a3585697af74f839a2a1fbde498d4691d
szabinakov <szabinkov@gmail.com>
2020-08-13 14:08:49 +0100

    added search button

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,12 +3,22 @@ import '../styles/Search.css'
 
 
 const Search = () => {
+    const [value, setValue] = useState()
+
     return(
     <>
+    <form className='searchForm'>
         <input
             className='searchInput'
             type='text'>
         </input>
+        <button 
+            className='searchButton'
+            type='submit'
+            onChange={(e) => setValue(e.target.value)}>
+                Search!
+        </button>
+    </form>
     </>
     )
 }
